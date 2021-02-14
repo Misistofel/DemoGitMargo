8d9eeb44ef027d66e3ad26a97af24e3af4477b19
margo <marinatofel@gmail.com>
2021-02-14 13:11:45 +0200

    first commit

---
+++ src/main/java/ru/stqa/pft/Sandbox2020/MyFirstProgram2020.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.Sandbox2020;
+
+public class MyFirstProgram2020 {
+
+  public static void main(String[] args) {
+
+    System.out.println("Hello World!");
+    hello("Margo");
+  }
+
+    public static void hello(String name) {
+    System.out.println("Hello, "+ name);
+  }
+
+
+}
